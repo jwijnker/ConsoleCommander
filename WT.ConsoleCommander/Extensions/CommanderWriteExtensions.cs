b81b32858b54bf7a0304363dc15784aa49e470d2
Jim Wijnker <jim.wijnker@hotmail.com>
2020-12-08 10:04:42 +0100

    Added WriteToTable as commander extension.

--- WT.ConsoleCommander/Extensions/CommanderWriteExtensions.cs
+++ WT.ConsoleCommander/Extensions/CommanderWriteExtensions.cs
@@ -65,5 +65,72 @@ namespace WT.ConsoleCommander
             commander.Info($"There are {items.Count()} items found.");
             commander.WriteEmptyLine();
         }
+
+        public static void WriteAsTable<T>(this CommanderBase commander, IEnumerable<T> items, IDictionary<string, Func<T, object>> columns, string nullReplacementValue = "NULL")
+        {
+            // Dictionary to hold the max lenght of a column-value.
+            var lenDict = new Dictionary<string, int>();
+
+            foreach (var c in columns)
+            {
+                // Determine the max lenght of values of the column
+                var len = ((items == null | !items.Any())
+                        ? 0
+                        : items.Max(i => {
+                            var r = c.Value.Invoke(i);
+                            return (r == null) ? nullReplacementValue.Length : r.ToString().Length;
+                        })
+                        );
+
+                // Store the max len
+                lenDict.Add(c.Key, Math.Max(len, c.Key.Length));
+            }
+
+            // Title
+            var lenTotal = lenDict.Values.Sum()
+                + ((lenDict.Count() -1) * 3);
+            var titleLen = typeof(T).Name.Length;
+
+            commander.WriteLine($"| {typeof(T).Name}{new String(' ', lenTotal - titleLen)} |", ConsoleColor.White);
+
+            // Table Headers
+            foreach (var header in columns)
+            {
+                var len = lenDict[header.Key];
+                commander.Write($"| {header.Key.PadRight(len)} ", ConsoleColor.White);
+            }
+            commander.WriteLine($"|", ConsoleColor.White);
+
+            // Separator-line/row
+            foreach (var i in columns)
+            {
+                commander.Write($"|{new String('=', lenDict[i.Key] + 2)}");
+            }
+            commander.WriteLine($"|");
+
+            // Values
+            foreach (var i in items)
+            {
+                foreach (var c in columns)
+                {
+                    // Get the value first
+                    var result = c.Value.Invoke(i);
+                    // .. check if not null
+                    //var value = (result == null) ? string.Empty : result;
+                    var value = (result == null)
+                        ? nullReplacementValue
+                        : result.ToString()
+                            .Replace(Environment.NewLine, string.Empty);
+
+                    Console.Write($"| {value.ToString().PadRight(lenDict[c.Key])} ");
+                }
+                Console.WriteLine($"|");
+            }
+
+            // Footer
+            var message = $"Lines: {items.Count()}, Columns: {columns.Count}";
+            commander.WriteLine($"| {message}{new String(' ', lenTotal - message.Length)} |", ConsoleColor.White);
+        }
+
     }
 }
