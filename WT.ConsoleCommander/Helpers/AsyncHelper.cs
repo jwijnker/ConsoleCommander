826c0fa485c67b789d91925a062cf5c017fc766d
Jim Wijnker <jim.wijnker@hotmail.com>
2020-12-08 14:03:24 +0100

    Added Asynchronous Helper.

---
+++ WT.ConsoleCommander/Helpers/AsyncHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace WT.ConsoleCommander.Helpers
+{
+    [DebuggerStepThrough]
+    public static class AsyncHelper
+    {
+        public static T ExecuteAsync<T>(this Task<T> task)
+        {
+            //return task.GetAwaiter().GetResult();
+            return AsyncHelper.ExecuteAsync(task, 10);
+        }
+
+        public static void ExecuteAsync(this Task task)
+        {
+            task.GetAwaiter().GetResult();
+        }
+
+        public static T ExecuteAsync<T>(this Task<T> task, int timeout)
+        {
+            var result = task.GetAwaiter().GetResult();
+
+            Task.Delay(timeout);
+
+            return result;
+        }
+    }
+}
