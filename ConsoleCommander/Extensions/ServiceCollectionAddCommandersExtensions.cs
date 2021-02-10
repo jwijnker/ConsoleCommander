db84b123935c5aba3e8f8662fa5736619d156ad8
Jim Wijnker <jim.wijnker@hotmail.com>
2021-02-10 22:33:46 +0100

    Improved (extension-) method to determine if a class is a commander. Removed commented code.

--- ConsoleCommander/Extensions/ServiceCollectionAddCommandersExtensions.cs
+++ ConsoleCommander/Extensions/ServiceCollectionAddCommandersExtensions.cs
@@ -1,18 +1,13 @@
-﻿using ConsoleCommander;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 
 namespace ConsoleCommander.Extensions
-//namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ServiceCollectionAddCommandersExtensions
     {
-        //public static IServiceCollection AddCommanders(this IServiceCollection serviceCollection)
-        //{
-        //    return AddCommanders(serviceCollection, typeof(DependencyInjection).Assembly);
-        //}
-
         public static IServiceCollection AddCommanders(this IServiceCollection serviceCollection, Assembly assembly)
         {
             return AddCommanders(serviceCollection, new[] { assembly });
@@ -23,20 +18,32 @@ namespace ConsoleCommander.Extensions
             foreach (var a in assemblies)
             {
                 var types = a.GetTypes()
-                    .Where(e => e.DeclaringType != null)
-                    .Where(x => x.IsClass && (x.BaseType.IsAssignableFrom(typeof(CommanderBase)) | x.BaseType.IsAssignableFrom(typeof(CommanderBase<object>))))
-                   .OrderBy(e => e.FullName)
-                   .Select(e => e.DeclaringType)
-                    .Distinct()
-               ;
+                    .Where(t => t.IsCommander())
+                ;
 
                 foreach (var type in types)
                 {
+                    Debug.WriteLine($"Register commander '{type.Name}'.");
                     serviceCollection.AddTransient(type, type);
                 }
             }
 
             return serviceCollection;
+        }
+
+        public static bool IsCommander(this Type type)
+        {
+            if (type.BaseType == typeof(CommanderBase))
+            {
+                return true;
+            }
+
+            if (type.BaseType == typeof(object))
+            {
+                return false;
+            }
+
+            return IsCommander(type.BaseType);
         }
     }
 }
